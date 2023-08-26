b86fc999614bf52bef894032cec9d8f8faa88a1c
Nino Carrillo <nino.carrillo@outlook.com>
2023-08-26 06:18:45 -0400

    add report file

--- rrc-gen.py
+++ rrc-gen.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import math
 import numpy as np
@@ -36,10 +37,21 @@ def InitRRCFilter(this):
 		index += 1
 	this['Taps'] = this['Taps'] / np.linalg.norm(this['Taps'])
 	this['RC'] = np.convolve(this['Taps'], this['Taps'], 'same')
-
-	N = this['TapCount'] - 1
-
 	return this
+	
+def GenInt16ArrayC(name, array, column_width):
+	result = '\n'
+	result += f'const __prog__ int16_t __attribute__((space(prog))) {name}[{len(array)}] = '
+	result += '{ '
+	y = len(array)
+	for x in range(y):
+		if x % column_width == 0:
+			result += ' \\\n     '
+		result += f' {int(np.rint(array[x]))}'
+		if x < (y-1):
+			result += ','
+	result += ' };'
+	return result
 
 if len(sys.argv) < 5:
 		print("Not enough arguments. Usage: python3 rrc-gen.py <rolloff rate> <span> <samples per symbol> <amplitude>")
@@ -52,7 +64,40 @@ filter['samples per symbol'] = int(sys.argv[3])
 filter['amplitude'] = int(sys.argv[4])
 
 filter = InitRRCFilter(filter)
+filter['ScaledTaps'] = np.rint(filter['Taps'] * filter['amplitude'])
 plt.figure()
-plt.plot(filter['Taps'])
+plt.plot(filter['ScaledTaps'])
 plt.title('RRC')
 plt.show()
+
+#generate a new director for the reports
+run_number = 0
+print('trying to make a new directory')
+while True:
+	run_number = run_number + 1
+	dirname = f'./run{run_number}/'
+	try:
+		os.mkdir(dirname)
+	except:
+		print(dirname + ' exists')
+		continue
+	print(f'made new directory {dirname}')
+	break
+
+# Generate and save report file
+report_file_name = f'run{run_number}_report.txt'
+try:
+	report_file = open(dirname + report_file_name, 'w+')
+except:
+	print('Unable to create report file.')
+with report_file:
+	report_file.write('# Command line: ')
+	for argument in sys.argv:
+		report_file.write(f'{argument} ')
+
+	report_file.write('\n\n# RRC Pulse Filter\n')
+	report_file.write('\n')
+	report_file.write(GenInt16ArrayC(f'RRCFilter', filter['ScaledTaps'], filter['samples per symbol']))
+	report_file.write('\n')
+	report_file.close()
+	print(f'wrote {dirname + report_file_name}')
